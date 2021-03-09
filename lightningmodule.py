931aaefa9bb403491793d723a86d45a379bdfec1
Ákops Gángoly <gangoly96vgmail.com>
2021-03-09 19:15:13 +0000

    no metrics on progressbar

--- lightningmodule.py
+++ lightningmodule.py
@@ -33,46 +33,18 @@ class MULTModelWarped(pl.LightningModule):
         self.log_dict(
             metric_values, on_step=True, on_epoch=True, prog_bar=False, logger=True
         )
-        self.log(
-            "train_loss",
-            metric_values["train_loss"],
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-            logger=False,
-        )
-        self.log(
-            "train_1mae",
-            metric_values["train_1mae"],
-            on_step=True,
-            on_epoch=True,
-            prog_bar=True,
-            logger=False,
-        )
         return metric_values["train_loss"]
 
     def validation_step(self, batch, batch_idx):
         metric_values = self._calc_loss_metrics(batch)
         metric_values = {f"valid_{k}": v for k, v in metric_values.items()}
         self.log_dict(metric_values, prog_bar=False, logger=True)
-        self.log(
-            "valid_loss", metric_values["valid_loss"], prog_bar=True, logger=False,
-        )
-        self.log(
-            "valid_1mae", metric_values["valid_1mae"], prog_bar=True, logger=False,
-        )
         return metric_values
 
     def test_step(self, batch, batch_idx):
         metric_values = self._calc_loss_metrics(batch)
         metric_values = {f"test_{k}": v for k, v in metric_values.items()}
         self.log_dict(metric_values, prog_bar=False, logger=True)
-        self.log(
-            "test_loss", metric_values["test_loss"], prog_bar=True, logger=False,
-        )
-        self.log(
-            "test_1mae", metric_values["test_1mae"], prog_bar=True, logger=False,
-        )
         return metric_values
 
     def _calc_loss_metrics(self, batch):
