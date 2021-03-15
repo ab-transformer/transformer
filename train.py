c37e2bf47e6dd35162fa63c3afac4bcaa5954113
matech96 <gangoly96@gmail.com>
2021-03-15 15:54:05 +0000

    trainer refine

--- train.py
+++ train.py
@@ -140,13 +140,13 @@ train_dl = th.utils.data.DataLoader(
 )
 valid_dl = th.utils.data.DataLoader(
     valid_ds, batch_size=hyp_params.batch_size, pin_memory=True,
+hyp_params.orig_d_l = text.shape[2]
 )
 test_dl = th.utils.data.DataLoader(
     test_ds, batch_size=hyp_params.batch_size, pin_memory=True,
 )
 
 audio, face, text, label = next(iter(train_dl))
-hyp_params.orig_d_l = text.shape[2]
 hyp_params.orig_d_a = audio.shape[2]
 hyp_params.orig_d_v = face.shape[2]
 hyp_params.l_len = text.shape[1]
@@ -164,28 +164,29 @@ hyp_params.output_dim = label.shape[1]  # output_dim_dict.get(dataset, 1)
 model = MULTModelWarped(hyp_params, target_names)
 
 if __name__ == "__main__":
-    for lr in [0.0001, 0.00001]:  # 0.1, 0.01, 0.001, 
-        hyp_params.lr = lr
-        model = MULTModelWarped(hyp_params, target_names)
-        comet_logger = CometLogger(
-            api_key="cgss7piePhyFPXRw1J2uUEjkQ",
-            workspace="transformer",
-            project_name="compare_loss",
-            save_dir="logs/comet_ml",
-        )
-        csv_logger = CSVLogger("logs/csv", name=comet_logger.experiment.get_key())
-        trainer = pl.Trainer(
-            gpus=1,
-            max_epochs=hyp_params.num_epochs,
-            log_every_n_steps=1,
-            callbacks=[
-                EarlyStopping(
-                    monitor="valid_1mae", patience=10, verbose=True, mode="max"
-                )
-            ],
-            logger=[csv_logger, comet_logger],
-            limit_train_batches=hyp_params.limit,
-            limit_val_batches=hyp_params.limit,
-        )
-        trainer.fit(model, train_dl, valid_dl)
-        trainer.test(test_dataloaders=test_dl)
+    hyp_params.lr = lr
+    model = MULTModelWarped(hyp_params, target_names)
+    comet_logger = CometLogger(
+        api_key="cgss7piePhyFPXRw1J2uUEjkQ",
+        workspace="transformer",
+        project_name="compare_loss",
+        save_dir="logs/comet_ml",
+    )
+    csv_logger = CSVLogger("logs/csv", name=comet_logger.experiment.get_key())
+    trainer = pl.Trainer(
+        gpus=1,
+        max_epochs=hyp_params.num_epochs,
+        log_every_n_steps=1,
+        callbacks=[
+            EarlyStopping(
+                monitor="valid_1mae", patience=10, verbose=True, mode="max"
+            )
+        ],
+        logger=[csv_logger, comet_logger],
+        limit_train_batches=hyp_params.limit,
+        limit_val_batches=hyp_params.limit,
+        weights_summary='full',
+        weights_save_path='logs/weights'
+    )
+    trainer.fit(model, train_dl, valid_dl)
+    trainer.test(test_dataloaders=test_dl)
