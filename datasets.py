74752f6a7cf18f3e52c0a37e2be5b1a38a41ea78
matech96 <gangoly96@gmail.com>
2021-06-20 15:23:01 +0000

    fix

--- datasets.py
+++ datasets.py
@@ -23,7 +23,7 @@ REPORT_IMPRESSIONV2_DIR = Path("/workspace/lld_au_bert")
 
 class ReportImpressionV2DataSet(th.utils.data.Dataset):
     def __init__(self, data, trfs):
-        self.data = map(self.process_data, data)
+        self.data = list(map(self.process_data, data))
         self.trfs = trfs
 
     def process_data(self, item):
